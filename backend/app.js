0538bb7d429db4575b64417618a0ffa8e4ef0c35
Abhishek <abhishekkdk.2022@gmail.com>
2026-06-04 18:31:32 +0545

    backend env fixed

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "FRONTEND_BASE_URL",
+    origin: process.env.FRONTEND_BASE_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
